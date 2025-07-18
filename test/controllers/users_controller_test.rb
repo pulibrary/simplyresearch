7435da53770d9e8743d6772873652efed2bc4a79
jrgriffiniii <1443986+jrgriffiniii@users.noreply.github.com>
2025-07-18 11:24:44 -0400

    Integrating SimpleCov for test code coverage, ESLint for JavaScript linting, and Prettier for formatting

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,7 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @user = users(:one)
-  end
+  setup { @user = users(:one) }
 
   test "should get index" do
     get users_url
@@ -17,7 +15,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create user" do
     assert_difference("User.count") do
-      post users_url, params: { user: { email: @user.email, first_name: @user.first_name, last_name: @user.last_name } }
+      post users_url,
+           params: {
+             user: {
+               email: @user.email,
+               first_name: @user.first_name,
+               last_name: @user.last_name
+             }
+           }
     end
 
     assert_redirected_to user_url(User.last)
@@ -34,14 +39,19 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update user" do
-    patch user_url(@user), params: { user: { email: @user.email, first_name: @user.first_name, last_name: @user.last_name } }
+    patch user_url(@user),
+          params: {
+            user: {
+              email: @user.email,
+              first_name: @user.first_name,
+              last_name: @user.last_name
+            }
+          }
     assert_redirected_to user_url(@user)
   end
 
   test "should destroy user" do
-    assert_difference("User.count", -1) do
-      delete user_url(@user)
-    end
+    assert_difference("User.count", -1) { delete user_url(@user) }
 
     assert_redirected_to users_url
   end
