db207bf199f143abc4427e522d11f808456f0098
jrgriffiniii <1443986+jrgriffiniii@users.noreply.github.com>
2025-07-22 14:51:06 -0400

    Adding the improvements proposed by Copilot

--- app/javascript/entrypoints/application.jsx
+++ app/javascript/entrypoints/application.jsx
@@ -4,13 +4,15 @@ import ReactDOM from "react-dom/client";
 import App from "../components/App.jsx";
 
 const container = document.getElementById("react-root");
-const children = Array.from(container.children).map((child) =>
-  React.createElement("div", {
-    dangerouslySetInnerHTML: { __html: child.outerHTML },
-  }),
-);
+let children = [];
 
 if (container) {
+  children = Array.from(container.children).map((child) =>
+    React.createElement("div", {
+      dangerouslySetInnerHTML: { __html: child.outerHTML },
+    }),
+  );
+
   const root = ReactDOM.createRoot(container);
   root.render(
     <React.StrictMode>
